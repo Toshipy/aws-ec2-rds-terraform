5278ef8804e1e42dab711e4f1b7298a2e8308553
Toshipy <juantoshi1209@gmail.com>
2023-01-14 10:27:44 +0900

    create vpc & subnet

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
   profile = "arai_toshiaki"
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
 
-    default_tags {
+  default_tags {
     tags = {
       aws-exam-resource = "true"
     }
   }
 
   # assume_roleブロックを追加
-  assume_role {  
+  assume_role {
     role_arn = "arn:aws:iam::170775015612:role/EngineedExam00374"
   }
 }
 
 resource "aws_instance" "hello-world" {
-  ami = "ami-01e94099fb3acf7fa"
+  ami           = "ami-01e94099fb3acf7fa"
   instance_type = "t2.micro"
 }
