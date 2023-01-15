3d64c361dc8f7635965998b5ae436704077c7b7d
Toshipy <juantoshi1209@gmail.com>
2023-01-15 12:18:46 +0900

    cloudfron sg changed https & rds instance

--- sg.tf
+++ sg.tf
@@ -17,14 +17,25 @@ resource "aws_security_group_rule" "alb_in_http" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-# resource "aws_security_group_rule" "alb_out_tcp3000" {
-#   security_group_id = aws_security_group.alb_sg.id
-#   type = "ingress"
-#   protocol = "tcp"
-#   from_port = 80
-#   to_port = 80
-#   cidr_blocks = [ "0.0.0.0/0" ]
-# }
+
+resource "aws_security_group_rule" "alb_in_https" {
+  security_group_id = aws_security_group.alb_sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "alb_out_tcp" {
+  security_group_id = aws_security_group.alb_sg.id
+  type              = "egress"
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 
 # EC2 security group
 resource "aws_security_group" "ec2_sg" {
